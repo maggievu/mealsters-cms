3b003c3d5b8ccdf8e92735646fd8eba64aedd689
Maggie <maggievu91@gmail.com>
2018-07-27 05:27:48 -0700

    complete 90% site

--- header.php
+++ header.php
@@ -110,8 +110,8 @@
                 <?php elseif ( is_archive() ) : ?>
                     <header class="page-header">
                         <?php
-                        the_archive_title( '<h1 class="page-title">', '</h1>' );
-                        the_archive_description( '<div class="archive-description">', '</div>' );
+                            the_archive_title( '<h1 class="page-title">', '</h1>' );
+                            the_archive_description( '<div class="archive-description">', '</div>' );
                         ?>
                     </header><!-- .page-header -->
 
